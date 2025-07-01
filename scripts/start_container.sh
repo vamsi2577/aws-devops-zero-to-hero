6f4d533d44dcfc7f0f7a134ed75cfba8aa04acf4
Vamsi chowdary <63102632+vamsi2577@users.noreply.github.com>
2025-07-01 08:27:35 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull vamsi109/simple-python-flask-app
-
+docker pull vamsi109/simple-python-app:latest
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vamsi109/simple-python-flask-app
+docker run -d -p 5000:5000 vamsi109/simple-python-app
